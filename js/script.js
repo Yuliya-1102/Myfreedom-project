94f9e5cdb9f40df777f05f70558fad18eedc21cc
Babitskaya <bambina-kissi@yandex.ru>
2023-05-19 23:44:14 +0300

    add main slide

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $('.main__offer__parent').slick({
     infinite: true,
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     nextArrow: '<button type="button" class="slick-next main__offer__buttons">></button>',
     prevArrow: '<button type="button" class="slick-prev main__offer__buttons"><</button>',
     responsive: [
@@ -11,7 +11,7 @@ $('.main__offer__parent').slick({
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
         }
       },
@@ -19,7 +19,7 @@ $('.main__offer__parent').slick({
         breakpoint: 800,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToScroll: 1
         }
       },
       {
@@ -31,5 +31,15 @@ $('.main__offer__parent').slick({
       }
       
     ]
+  });
+
+  $('.main__wrapper').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
   });
 				
